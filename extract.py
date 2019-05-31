c2563e9f01fb1356c800dc92e41ff31f4bb78f72
AtlasHale <connor.a.h.h@gmail.com>
2019-05-30 23:12:12 -0700

    Python program + test gif and result at .01 duration

---
+++ extract.py
@@ -0,0 +1,41 @@
+import os, shutil
+import cv2
+import imageio
+from PIL import Image, GifImagePlugin
+
+
+def extractFrames(gif, outFolder):
+    files = os.listdir(".")
+    if gif+"_custom_speed" in files:
+        os.remove(gif+"_custom_speed")
+    os.mkdir(outFolder)
+    cap = cv2.VideoCapture(gif)
+    i = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret == 0:
+            break
+        cv2.imwrite(outFolder+'/'+str(i)+'.jpg', frame)
+
+        i+=1
+    cap.release()
+    cv2.destroyAllWindows() 
+
+def custom_speed(gif, image_folder, dur):
+    frames = []
+    files = os.listdir(image_folder)
+    temp = {}
+    for x in files:
+        temp[int(x[:-4])]=x
+    for frame in sorted (temp.keys()):
+        frames.append(imageio.imread(image_folder+'/'+temp[frame]))
+    imageio.mimwrite(gif+"_custom_speed", frames, format="gif" ,duration=dur)
+    shutil.rmtree(gif+'_frames')
+
+
+
+s = input("file name: ")
+dur = input("duration of each frame (in seconds): ")
+extractFrames(s, s+'_frames')
+custom_speed(s, s+'_frames', dur)
+
